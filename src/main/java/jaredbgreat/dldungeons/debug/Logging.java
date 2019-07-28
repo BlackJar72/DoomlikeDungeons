99e73c2793be2278a6ce0cd5d9ddfed70fd02dc0
Jared Blackburn (JaredBGreat) <BlackJar72@yahoo.com>
2019-07-28 14:22:28 -0400

    Added / updated DBlock class

---
+++ src/main/java/jaredbgreat/dldungeons/debug/Logging.java
@@ -0,0 +1,52 @@
+package jaredbgreat.dldungeons.debug;
+
+import org.apache.logging.log4j.core.Logger;
+
+/* 
+ * Doomlike Dungeons by is licensed the MIT License
+ * Copyright (c) 2014-2018 Jared Blackburn
+ */	
+
+
+import jaredbgreat.dldungeons.DoomlikeDungeons;
+
+/**
+ * A mostly unused wrapper for the Java logger, to make getting a logger easier.
+ * 
+ * @author Jared Blackburn
+ */
+
+public class Logging {
+	private static Logger logger;
+	private static Logging log;
+	
+	private Logging() {
+		logger = (Logger)DoomlikeDungeons.logger;
+	}
+	
+	
+	public static Logging getInstance() {
+		if(log == null) {
+			log = new Logging();
+		}
+		return log;
+	}
+	
+	
+	public static void LogError(String error) {
+		if(log == null) {
+			log = new Logging();
+		}
+		System.err.println(error);
+		logger.error(error);;
+	}
+	
+	
+	public static void LogInfo(String info) {
+		if(log == null) {
+			log = new Logging();
+		}
+		logger.info(info);
+	}
+
+}
