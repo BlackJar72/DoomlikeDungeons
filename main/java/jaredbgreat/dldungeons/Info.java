1ff88ab5b471b7ab83bb1df75b012d5190d91e47
Jared Blackburn <BlackJar72@yahoo.com>
2021-10-27 03:18:17 -0400

    Returned accidentally deleted potions for oceanic dungeons

--- main/java/jaredbgreat/dldungeons/Info.java
+++ main/java/jaredbgreat/dldungeons/Info.java
@@ -12,7 +12,7 @@ public final class Info {
 	public static final String OLD_ID = "DLDungeonsJBG";
 	public static final String ID = "dldungeonsjbg";
 	public static final String NAME = "Doomlike Dungeons";
-	public static final String VERSION = "1.14.9";
+	public static final String VERSION = "1.14.10";
 	public static final String MINECRAFT = "1.12.2";
 	public static final String CHANNEL =  "JBGDungeons";
 	public static final String LOG_NAME = "DLDUNGEONS";
