bb22d53c8588dd9c8a49f7fc975b9a9893193476
Jared Blackburn <BlackJar72@yahoo.com>
2025-02-02 18:17:26 -0500

    Improvements to BlockFamily handling with data packs

--- src/main/java/jaredbgreat/dldungeons/builder/RegisteredBlock.java
+++ src/main/java/jaredbgreat/dldungeons/builder/RegisteredBlock.java
@@ -23,6 +23,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public final class RegisteredBlock extends AbstractBlock {
     private final String id;   // The name
@@ -34,9 +35,9 @@ public final class RegisteredBlock extends AbstractBlock {
     public static final Block air = Blocks.AIR;
 
     // All blocks, complete with meta-data used by the mod
-    public static final ArrayList<RegisteredBlock> registry = new ArrayList<RegisteredBlock>();
+    public static final CopyOnWriteArrayList<RegisteredBlock> registry = new CopyOnWriteArrayList<>();
 
-    private static HashSet<String> names = new HashSet<>();
+    private final static HashSet<String> names = new HashSet<>();
 
     /**
      * Gets the item named by the string "in" -- hacky, but might work
